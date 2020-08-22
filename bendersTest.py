692fc43f3ee316aed6a39b4e350f0b413467616b
Pramesh Kumar <kumar372@umn.edu>
2020-08-21 19:16:35 -0500

    first commit to this repository

---
+++ bendersTest.py
@@ -0,0 +1,230 @@
+'''
+This script is written by Pramesh Kumar for testing various acceleration techniques for Benders Decomposition
+
+Problem:
+
+    
+MAXIMIZE      \sum_{i \in C} \sum_{j \in F} p_{ij} y_{ij} - \sum_{j \in F} x_j
+s.t.          \sum_{j \in F} y_{ij} = 1, \forall i \in C
+              0 <= y_{ij} <= x_j, \forall i \in C, \forall j in F
+              x binary, y binary
+'''
+
+
+import numpy as np
+from gurobipy import *
+import time
+
+
+
+
+
+
+def generateFacilityLocationData(C, F):
+    # Unbounded ray instance seed 159
+    np.random.seed(15645)
+    p =  np.random.randint(1000, size=(C, F))
+    f = np.random.randint(1000, size=(F))
+    for j in range(F):
+        for i in range(C):
+            f[j] += round(0.05*p[i,j])
+
+    return C, F, p, f
+
+
+# Step 1: Initialize variables
+C = 100
+F = 50
+
+
+
+# Step 2: Start clock
+ts = time.time()
+# Step 3: Generate instance
+C, F, p, f = generateFacilityLocationData(C,F)
+
+############################################################################################################################
+##############################################################################################################################
+
+
+
+def solveModelGurobi():
+    m2 = Model()
+    x = {j: m2.addVar(lb=0, vtype=GRB.BINARY) for j in range(F)}
+    y = {(i, j): m2.addVar(lb=0, vtype=GRB.BINARY) for i in range(C) for j in range(F)}
+    for i in range(C):
+        m2.addConstr(sum([y[i, j] for j in range(F)]) == 1)
+    for j in range(F):
+        for i in range(C):
+            m2.addConstr(y[i, j] <= x[j])
+    obj = 0
+    for j in range(F):
+        obj = obj -f[j] * x[j]
+        for i in range(C):
+            obj += p[i, j] * y[i, j]
+    m2.setObjective(obj, sense=GRB.MAXIMIZE)
+    m2.update()
+    m2.Params.OutputFlag = 0
+    m2.setParam('OutputFlag', False)
+    m2.optimize()
+    xVal= [x[j].x for j in range(F)]
+    yVal =[y[i, j].x for i in range(C) for j in range(F)]
+    return m2.objVal + sum([xVal[j] * f[j] for j in range(F)]), xVal, yVal
+
+
+def subProblem(x):
+    m1 = Model()
+    y = {(i, j):  m1.addVar(lb=0, vtype=GRB.CONTINUOUS) for i in range(C) for j in range(F)}
+    constrMu = {}
+    constrNu = {}
+    for i in range(C):
+        constrMu[i] = m1.addConstr(sum([y[i, j] for j in range(F)]) == 1)
+    for j in range(F):
+        for i in range(C):
+            constrNu[i, j] = m1.addConstr(y[i, j] <= x[j])
+
+    obj = sum([p[i, j] * y[i, j]  for j in range(F) for i in range(C)]) -sum([f[j] * x[j] for j in range(F)])
+
+    m1.setObjective(obj, sense=GRB.MAXIMIZE)
+
+    m1.update()
+    m1.Params.OutputFlag = 0
+    m1.Params.InfUnbdInfo = 1
+    m1.Params.DualReductions = 0
+
+    m1.optimize()
+
+    if m1.status == GRB.OPTIMAL:
+        mu = {}
+        nu = {}
+        for i in range(C):
+            mu[i] = constrMu[i].pi
+        for j in range(F):
+            for i in range(C):
+                nu[i, j] = constrNu[i, j].pi
+        return m1.objVal, mu, nu, [y[i, j].x for i in range(C) for j in range(F)], m1.status
+    else:
+        mu = {}
+        nu = {}
+
+        for i in range(C):
+            mu[i] = constrMu[i].FarkasDual
+        for j in range(F):
+            for i in range(C):
+                nu[i, j] = constrNu[i, j].FarkasDual
+                ind += 1
+        return -float("inf"), mu, nu, [], m1.status
+
+
+def solveMaster(optCuts_mu, optCuts_nu, fesCuts_mu, fesCuts_nu, xhat):
+    m = Model()
+    eta =  m.addVar(vtype=GRB.CONTINUOUS)
+    x = {j: m.addVar(lb=0, vtype=GRB.BINARY) for j in range(F)}
+    '''
+    Adding optimality cuts
+    '''
+    
+    for k in range(len(optCuts_mu)):
+        tot = sum(optCuts_mu[k].values())
+
+        for j in range(F):
+            for i in range(C):
+                tot += optCuts_nu[k][i, j]*x[j]
+
+        m.addConstr(eta <= tot)
+    '''
+    Adding feasibility cuts
+    '''
+    for k in range(len(fesCuts_mu)):
+        tot = sum(fesCuts_mu[k].values())
+        for j in range(F):
+            for i in range(C):
+                tot += fesCuts_nu[k][i, j] * x[j]
+        m.addConstr(tot >= 0)
+        
+
+    m.setObjective(eta -sum([f[j] * x[j] for j in range(F)]), sense=GRB.MAXIMIZE)
+    m.update()
+    m.Params.OutputFlag = 0
+    m.optimize()
+    if m.status == GRB.OPTIMAL:
+        return m.objVal, [x[k].x for k in range(F)], eta.x
+    else:
+        print("Sth went wrong in the master problem and it is ", m.status)
+
+
+
+
+
+def solveUFL(eps, x_initial, maxit):
+    UB = float("inf")
+    LB = -float("inf")
+    optCuts_mu = []
+    optCuts_nu = []
+    fesCuts_mu = []
+    fesCuts_nu = []
+    tol = float("inf")
+    it = 0
+    x = x_initial
+
+    while eps < tol  and it < maxit :
+        ob, mu, nu, y, status = subProblem(x)
+        LB = max(LB, ob)
+        if status == 2:
+            #print(status, "optimality")
+            optCuts_mu.append(mu)
+            optCuts_nu.append(nu)
+        else:
+            #print(status, "feasibility")
+            fesCuts_mu.append(mu)
+            fesCuts_nu.append(nu)
+        obj, x, eta = solveMaster(optCuts_mu, optCuts_nu, fesCuts_mu, fesCuts_nu, x)
+        UB = min(UB, obj)
+
+        tol = UB - LB
+        it += 1
+        print('----------------------iteration '  + str(it) +'-------------------' )
+        print ('LB = ', LB, ', UB = ', UB, ', tol = ', tol)
+        if len([k for k in range(F) if round(x[k]) != 0]) != 0:
+            print('Opened Facilities: \t ', [k for k in range(F) if round(x[k]) != 0])
+        else:
+            print('No open facilities')
+        print('Assignment....')
+        '''
+        for i in range(C):
+            print('Customer \t | \t Facility')
+            print(str(i)+ '\t | \t ' + str([k for k in for j in range(F) for i in range(C) if y[0] == i and y[k] == 1][0]))
+        print(LB, UB, tol, x, it)
+        '''
+    return x, y
+
+
+def checkGurobiBendersSimilarity(xb, yb, xg, yg):
+
+    ind = 0
+    for j in range(F):
+        if xb[j] != xg[j]:
+            ind += 1
+    k = 0
+    for j in range(F):
+        for i in range(C):
+            if yb[k] != yg[k]:
+                ind += 1
+            k += 1
+    if ind == 0:
+        print('Solution obtained from both methods are same')
+    else:
+        print('Solution obtained from both methods are different!!')
+
+
+
+x_initial = np.zeros(F)
+x_initial[1] = 1
+x_initial[2] = 0
+start = time.time()
+xb, yb = solveUFL(10, x_initial, 1000)
+print("Benders took...", round(time.time() - start, 2), "seconds")
+start = time.time()
+obj, xg, yg = solveModelGurobi()
+print("Gurobi took...", round(time.time() - start, 2), "seconds")
+checkGurobiBendersSimilarity(xb, yb, xg, yg)
